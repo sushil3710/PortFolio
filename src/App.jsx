7e90ff8c29f98bb5bc532d7f829a83b94dcfa8b4
sushil3710 <2020csb1132@iitrpr.ac.in>
2023-09-15 02:44:45 +0530

    Pic

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
+      <div style={{ marginBottom: '-170px' }} className='bg-hero-pattern bg-cover bg-no-repeat bg-center mb-0'>
+  <Navbar />
+  <Hero />
+</div>
         <About />
         <Experience />
         <Tech />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,21 @@
 import { motion } from "framer-motion";
+import yourImage from "./myimage.jpg";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative w-full h-screen bottom-150  mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]   max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 mb-8`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div>
+        <div >
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Sushil Kumar</span>
           </h1>
@@ -22,12 +23,23 @@ const Hero = () => {
           I am a fourth-year student, currently pursuing my B.Tech in Computer Science and Engineering (CSE) at the esteemed institution, IIT Ropar.<br className='sm:block hidden' />
           I am excited to extend a warm welcome to you as you explore my portfolio
           </p>
+          <motion.div
+  className="flex justify-center items-center mt-5"
+  initial={{ opacity: 0, y: -20 }} // Initial animation state
+  animate={{ opacity: 1, y: 0 }} // Animation on load
+  transition={{ duration: 1 }} // Animation duration
+>
+  <img src={yourImage} alt='Your Image' className='w-96 h-96 rounded-full' />
+</motion.div>
+
         </div>
+        
       </div>
 
-      <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      {/* <ComputersCanvas /> */}
+
+      {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
@@ -43,7 +55,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
